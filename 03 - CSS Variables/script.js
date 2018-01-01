b24af2e6be2ad17082f11f96724edda0a58a64c9
Patrick <patrick.x.rivera@gmail.com>
2018-01-01 14:32:58 -0500

    Added spacing functionality

--- 03 - CSS Variables/script.js
+++ 03 - CSS Variables/script.js
@@ -3,7 +3,7 @@ const base = document.querySelector('#base');
 const spacing = document.querySelector('#spacing');
 const blur = document.querySelector('#blur');
 
-function getValue(element) {
+function getVal(element) {
   return element.value;
 }
 
@@ -12,13 +12,24 @@ function calcBlur(inputVal) {
   return blurVal;
 }
 
-base.addEventListener('input', () => {
-  let currentVal = getValue(base);
-  root.style.setProperty('--base-color', `${currentVal}`)
+function calcSpacing(inputVal) {
+  let spacingVal = inputVal * 0.5;
+  return spacingVal;
+}
+
+spacing.addEventListener('input', () => {
+  let inputVal = getVal(spacing);
+  let spacingVal = calcSpacing(inputVal);
+  root.style.setProperty('--spacing', `translate(${spacingVal}px, ${spacingVal}px)`);
 });
 
 blur.addEventListener('input', () => {
-  let inputVal = getValue(blur);
+  let inputVal = getVal(blur);
   let blurVal = calcBlur(inputVal);
   root.style.setProperty('--blur', `blur(${blurVal}px)`);
 })
+
+base.addEventListener('input', () => {
+  let inputVal = getVal(base);
+  root.style.setProperty('--base-color', `${inputVal}`)
+});
