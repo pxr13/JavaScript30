74550a055533d43eefa4e42d97a20cbabc4cbef4
Patrick <patrick.x.rivera@gmail.com>
2018-01-01 14:13:55 -0500

    Added blur functionality

--- 03 - CSS Variables/script.js
+++ 03 - CSS Variables/script.js
@@ -1,7 +1,24 @@
-const spacing = document.querySelector('#spacing');
 const root = document.documentElement;
+const base = document.querySelector('#base');
+const spacing = document.querySelector('#spacing');
+const blur = document.querySelector('#blur');
+
+function getValue(element) {
+  return element.value;
+}
+
+function calcBlur(inputVal) {
+  let blurVal =  inputVal * 0.2; // 0.2 used to scale blur val in px
+  return blurVal;
+}
 
 base.addEventListener('input', () => {
-  let currentColor = base.value;
-  root.style.setProperty('--base-color', `${currentColor}`)
+  let currentVal = getValue(base);
+  root.style.setProperty('--base-color', `${currentVal}`)
 });
+
+blur.addEventListener('input', () => {
+  let inputVal = getValue(blur);
+  let blurVal = calcBlur(inputVal);
+  root.style.setProperty('--blur', `blur(${blurVal}px)`);
+})
